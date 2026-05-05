cf72bed9936c3d9cdc6cf254a64893aaa6f3f4cc
williammmm <me@williammmm.dev>
2026-05-05 17:37:47 +0100

    feat(eww): add dynamic icons to volume

--- eww/scripts/vol_listener.sh
+++ eww/scripts/vol_listener.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+
+
 get_vol() {
     local status=$(wpctl get-volume @DEFAULT_SINK@)
 
     if [[ $status == *"[MUTED]"* ]]; then
-        echo "MUTED"
+        echo "󰖁  MUTED"
     else
-        echo "$status" | awk '{print int($2 * 100) "%"}'
+        local vol_num=$(awk '{print int($2 * 100)}' <<< "$status")
+        if (($vol_num == 0)) ; then
+            echo "󰝟  $vol_num%"
+        elif (( $vol_num < 25 )); then
+            echo "󰕿  $vol_num%"
+        elif (( $vol_num < 60 )); then
+            echo "󰖀  $vol_num%"
+        else
+            echo "󰕾  $vol_num%"
+        fi
     fi
 }
 
