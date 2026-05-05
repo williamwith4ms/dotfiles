c525bccfe95ce1b38d6c6ade29c8d5c75a42ff83
williammmm <me@williammmm.dev>
2026-05-05 18:07:06 +0100

    fix(eww): title showing null when no window open

--- eww/scripts/get_active_window.sh
+++ eww/scripts/get_active_window.sh
@@ -2,7 +2,6 @@
 
 socat -U - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do
     if echo "$line" | grep -q "^activewindow>>"; then
-        # When the window changes, get the new title
         hyprctl activewindow -j | jq -r '.title'
     fi
 done
