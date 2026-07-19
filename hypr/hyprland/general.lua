c5ee6ecd6fe520c815d2c43e95d3e8d22b992c68
williammmm <me@williammmm.dev>
2026-07-19 01:20:27 +0100

    feat: update monitor configurations and remove unused listener

--- hypr/hyprland/general.lua
+++ hypr/hyprland/general.lua
@@ -1,8 +1,15 @@
 hl.monitor({
-    output = "",
-    mode = "preferred",
-    position = "auto",
-    scale = "auto"
+    output = "DP-3",
+    mode = "2560x1440@165.00Hz",
+    position = "0x0",
+    scale = "1"
+})
+
+hl.monitor({
+    output = "DP-1",
+    mode = "1920x1080@60.00Hz",
+    position = "2560x240",
+    scale = "1"
 })
 
 hl.config({
