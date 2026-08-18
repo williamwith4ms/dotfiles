9594699c71da8feae29461e26ef2a92b38a1d70c
williammmm <me@williammmm.dev>
2026-08-18 19:13:46 +0100

    bring changes from win95 into main

--- hypr/hyprland/autostart.lua
+++ hypr/hyprland/autostart.lua
@@ -1,29 +1,31 @@
 require("hyprland/var")
 
 hl.on("hyprland.start", function()
+	hl.exec_cmd("protonvpn-app")
+	-- Bar
+	hl.exec_cmd("eww open bar0")
+	hl.exec_cmd("eww open bar1")
 
-    hl.exec_cmd("protonvpn-app")
-    -- Bar
-    hl.exec_cmd("eww open bar0")
-    hl.exec_cmd("eww open bar1")
+	-- workspace
+	hl.exec_cmd(wallpaper_setter .. wallpaper)
 
-    -- workspace
-    hl.exec_cmd(wallpaper_setter .. wallpaper)
+	-- notification daemon
+	hl.exec_cmd(notification_daemon)
 
-    -- notification daemon
-    hl.exec_cmd(notification_daemon)
+	-- wallet
+	hl.exec_cmd(wallet_pam)
+	hl.exec_cmd(wallet)
 
-    -- wallet
-    hl.exec_cmd(wallet_pam)
-    hl.exec_cmd(wallet)
+	-- cliphist
+	hl.exec_cmd("wl-paste --type image --watch cliphist store")
+	hl.exec_cmd("wl-paste --type text --watch cliphist store")
 
-    -- cliphist
-    hl.exec_cmd("wl-paste --type image --watch cliphist store")
-    hl.exec_cmd("wl-paste --type text --watch cliphist store")
-
-    -- startup applications
-    hl.exec_cmd("sleep 5 && keepassxc --minimized")
-    hl.exec_cmd("sleep 5 && protonmail-bridge --no-window")
-    hl.exec_cmd("sleep 5 && vesktop --user-agent \"Mozilla/5.0 (X11; Linux x86_64; rv:150.0) Gecko/20100101 Firefox/150.0\" ", { workspace = "special silent" })
-    hl.exec_cmd("sleep 30 && thunderbird", { workspace = "10 silent" })
+	-- startup applications
+	hl.exec_cmd("sleep 5 && keepassxc --minimized")
+	hl.exec_cmd("sleep 5 && protonmail-bridge --no-window")
+	hl.exec_cmd(
+		'sleep 5 && discord --user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:150.0) Gecko/20100101 Firefox/150.0" ',
+		{ workspace = "special:chat silent" }
+	)
+	hl.exec_cmd("sleep 30 && thunderbird", { workspace = "10 silent" })
 end)
--- hypr/hyprland/env.lua
+++ hypr/hyprland/env.lua
@@ -1,3 +1,6 @@
 
 hl.env("XCURSOR_SIZE", 24)
 hl.env("HYPRCURSOR_SIZE", 24)
+hl.env("GTK_THEME", "Chicago95")
+hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
