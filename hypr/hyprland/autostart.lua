208c9a8f66e91c826c2ec3ad33c671321a7fcaab
williammmm <me@williammmm.dev>
2026-09-22 11:59:38 +0100

    fix(hypr): update autostart

--- hypr/hyprland/autostart.lua
+++ hypr/hyprland/autostart.lua
@@ -4,8 +4,8 @@ require("hyprland/var")
 hl.on("hyprland.start", function()
 	hl.exec_cmd("protonvpn-app")
 	-- Bar
-	hl.exec_cmd("eww open hzBar0")
-	hl.exec_cmd("eww open vBar1")
+	hl.exec_cmd("eww open hzBar1")
+	hl.exec_cmd("eww open vBar0")
 
 	-- workspace
 	hl.exec_cmd(wallpaper_setter .. wallpaper)
